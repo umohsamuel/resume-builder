8e8154675819f23964f7de9177569e101a424899
Umoh Samuel <umohsg@gmail.com>
2025-05-23 01:24:13 +0100

    lebrooooooooon

--- src/router/routes/dashboard/layout.tsx
+++ src/router/routes/dashboard/layout.tsx
@@ -11,25 +11,21 @@ import { Outlet } from "react-router";
 export default function DashboardLayout() {
   const { isLoaded } = useAuth();
 
-  if (!isLoaded) {
-    return (
-      <div className="w-full h-full min-h-screen flex justify-center items-center">
-        <LoaderCircle size={28} color="blue" className="animate-spin" />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <SignedIn>
+        <Navbar />
+        {!isLoaded && (
+          <div className="w-full h-full min-h-screen flex justify-center items-center">
+            <LoaderCircle size={28} color="blue" className="animate-spin" />
+          </div>
+        )}
 
-  if (isLoaded) {
-    return (
-      <div>
-        <SignedIn>
-          <Navbar />
-          <Outlet />
-        </SignedIn>
-        <SignedOut>
-          <RedirectToSignIn />
-        </SignedOut>
-      </div>
-    );
-  }
+        {isLoaded && <Outlet />}
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </div>
+  );
 }
